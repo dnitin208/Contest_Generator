73affd9b0c01143f4b1d783c4b9a3e1833572849
dnitin208 <dnitin28@gmail.com>
2022-05-23 02:05:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express=require("express");
+const bodyparser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));  
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+app.listen(process.env.PORT || 3000,function(req,res){
+    console.log("Server is running on port 3000");
+})
